23738250e0127c60798f1ecd2f69099679b5398c
Isaac <isaactian.contact@gmail.com>
2023-05-01 01:03:14 -0400

    scene transition fading

--- impossibleEnvironment/Assets/Scripts/sceneSwitchTrigger.cs
+++ impossibleEnvironment/Assets/Scripts/sceneSwitchTrigger.cs
@@ -5,8 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class sceneSwitchTrigger : MonoBehaviour
 {
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
     private void OnTriggerEnter(Collider other)
     {
+        LoadNextLevel();
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
         SceneManager.LoadScene(1);
     }
 }
