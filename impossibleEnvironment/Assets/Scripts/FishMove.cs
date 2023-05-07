32cd200ce25c3f629c7f245c96cc9eddd3918212
kc-visuals <karlatovera@gmail.com>
2023-05-07 18:37:32 -0400

    added minigame

---
+++ impossibleEnvironment/Assets/Scripts/FishMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishMove : MonoBehaviour
+{
+    void Start()
+    {
+        // fish falls into toilet
+        // audio plays
+        // ui talls player what to do
+        // music plays
+    }
+
+    void Update()
+    {
+        // player must use e to interact and get fish out of toilet flush
+        // must consistently press e to save fish
+        // bar to fill?
+        // bar goes down if player stops pressing e
+        // if bar is below 0, lose screen plays > restart button
+
+        // if player fills bar, animation plays?
+        //
+    }
+}
